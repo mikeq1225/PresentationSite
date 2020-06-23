5c55cbc50088dfd977191c2a8239cf9df018d6d6
Michael Quarne <mikedq1225@gmail.com>
2020-06-23 13:25:56 -0700

    finished presentation site

---
+++ smoothScroll.js
@@ -0,0 +1,15 @@
+// Smooth Scrolling
+$("#navBar a, .btn").on("click", function (e) {
+	if (this.hash !== "") {
+		e.preventDefault()
+
+		const hash = this.hash
+
+		$("html, body").animate(
+			{
+				scrollTop: $(hash).offset().top,
+			},
+			800
+		)
+	}
+})
